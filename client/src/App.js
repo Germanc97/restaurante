06556232759165e3c1d604c2d95a844aeb6f48cb
mclopez09 <mclopez09@javerianacali.edu.co>
2019-10-19 17:42:07 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './LogoBlanco.png';
 import './App.css';
+import { Button, Container, Divider, Grid, Header, Image, Menu, Segment,Rating } from 'semantic-ui-react'
+import { makeStyles } from '@material-ui/core/styles';
+const ValueStars=3;
+const useStyles = makeStyles({
+  LogOut: {
+    marginTop: 10,
+    alignContent: "center",  
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Menu">
+        <img src={logo} className="App-logo" alt="logo"/>
+          <Button inverted basic>Información</Button>
+          <Button inverted basic>Reservas</Button>
+          <Button inverted basic>Buzón</Button>
+          <Button inverted basic>Galeria</Button>
+          <Button inverted basic>Eventos</Button>
+          <Button inverted basic>Decoración</Button>
+          <Button inverted basic>Comentarios</Button>
+          <Button inverted basic>Mesas</Button>
+        </div>
+        <div className="UserInformation">
+          Bienvenido:
+          UserName
+          <div><Rating icon='star' defaultRating={ValueStars} maxRating={ValueStars} disabled icon='star' size='huge'/></div>
+        </div>
+        <div><Button className={classes.LogOut} inverted basic>CS</Button></div>
       </header>
     </div>
   );
