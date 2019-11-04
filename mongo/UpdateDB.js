ca0e1d95aef6d961b113526b4840b0c980198724
David Hernández Cárdenas <davidher28@javerianacali.edu.co>
2019-11-04 13:13:21 -0500

    Add files via upload

--- mongo/UpdateDB.js
+++ mongo/UpdateDB.js
@@ -7,8 +7,9 @@ var update = {
 	name : "Rest Prueba2", 
 	description : "Muy lujoso", 
 	city_id : 2, 
-	addres : "Lejos de aquí", 
+	address : "Lejos de aquí", 
 	telephone : 12348756, 
-	email : "rest2@yopmail.com" }
+	email : "rest2@yopmail.com",
+ 	schedule : "Lun-Dom 10:00AM - 12:00AM" }
 dbo.collection("Restaurant").update(query,update);
 // ----------------------------------------------------------------------------------------
